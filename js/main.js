3c6e3a7312971c705a2a5165f994620bca07d700
mohammad moein abdi <moeinabdi77@gmail.com>
2018-12-01 22:47:41 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Mohammad Moein Abdi", "Developer", "Android Developer", "iOS Developer", "Teacher"],
       typeSpeed: 100,
       loop: true,
     });
