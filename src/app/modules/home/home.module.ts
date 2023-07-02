b88a8bad5b862d9c9e676541d32d3b61ef11cf3a
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2023-07-02 17:11:33 +0330

    feat(angular-rewrite): implement summary part

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { IntroductionModule } from '../introduction/introduction.module';
+import { SummaryModule } from '../summary/summary.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, IntroductionModule],
+  imports: [CommonModule, HomeRoutingModule, IntroductionModule, SummaryModule],
 })
 export class HomeModule {}
